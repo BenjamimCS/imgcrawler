7417fd1fecaa84fbcb703642481001a955c15f4c
BenjamimCS <uj1kjfomy@mozmail.com>
2024-07-01 03:24:17 -0300

    [revision] `utils.url` URL regex
    - fix: match algarism from 0 to 9 (before: 1 to 9)
    - optimize: each part of the URL are now in its own variable. It’s
      useful when searching specific parts so instead of getting all the
      string the pattern only searchs for an specific pattern on it

--- utils/url.py
+++ utils/url.py
@@ -1,22 +1,24 @@
 import re
 
-urlregex = r'(https?):\/\/((?:[a-z1-9%]+\.)?[a-z1-9%]+(?:\.[a-z]+)+)'
-urlpathregex = urlregex + r'(\/.*)?'
+urlschemeregex = r'(\w+)'
+urlpathregex = r'(\/.*)?'
+urlauthorityregex = r'\/\/((?:[a-z0-9%]+\.)?[a-z0-9%]+(?:\.[a-z]+)+)' # before path of scheme-specific-part
+urlregex = urlschemeregex + ':' + urlauthorityregex + urlpathregex
 
 def isurl(url:str) -> bool:
-  matches = re.fullmatch(urlpathregex, url)
+  matches = re.fullmatch(urlregex, url)
   if matches: return True
   else:       return False
 
 def gethost(url:str) -> str:
   if not isurl(url): raise ValueError('*url* is not a valid URL')
 
-  host = re.search(urlregex, url, re.IGNORECASE)
-  return host.group(2)
+  host = re.search(urlauthorityregex, url, re.IGNORECASE)
+  return host.group(1)
 
 def getprotocol(url:str) -> str:
-  protocol = re.match(urlregex, url, re.IGNORECASE)
-  return protocol.group(2)
+  protocol = re.match(urlschemeregex, url, re.IGNORECASE)
+  return protocol.group(1)
 
 def makeurl(site:str='', path:str = '', protocol:str = 'https'):
   pattern = re.search(urlpathregex, site)
